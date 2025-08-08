c14d7fe6fc255ded3c53aeb9cf0b31313f854522
Federico Betti <federico.betti@outlook.com>
2025-08-08 15:09:05 +0200

    Initial Commit

---
+++ PowerWatt/PowerWattApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerWattApp.swift
+//  PowerWatt
+//
+//  Created by federico betti on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PowerWattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerWattTests/PowerWattTests.swift
@@ -0,0 +1,17 @@
+//
+//  PowerWattTests.swift
+//  PowerWattTests
+//
+//  Created by federico betti on 08/08/25.
+//
+
+import Testing
+@testable import PowerWatt
+
+struct PowerWattTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
